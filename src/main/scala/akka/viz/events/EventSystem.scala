6c975d73abe1446be0d0c65ee73298a01d561e4f
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-01-21 14:01:52 +0100

    EventSystem to publish and handle subscription of events

---
+++ src/main/scala/akka/viz/events/EventSystem.scala
@@ -0,0 +1,57 @@
+package akka.viz.events
+
+import akka.actor.Actor.Receive
+import akka.actor._
+import akka.event.{ActorEventBus, EventBus}
+import akka.stream.Materializer
+import akka.stream.actor.ActorPublisher
+import akka.stream.scaladsl._
+import akka.viz.config.Config
+import org.reactivestreams.Publisher
+import scala.collection.immutable
+import akka.viz.util.FiniteQueue._
+
+
+sealed trait Event
+
+case class Received(sender: ActorRef, receiver: ActorRef, message: Any) extends Event
+
+
+object EventSystem {
+
+  case object Subscribe
+
+  case object Unsubscribe
+
+  private implicit val system = ActorSystem(Config.internalSystemName)
+  private val publisher = system.actorOf(Props(new Actor {
+    val maxElementsInQueue = Config.eventsToReply
+    var queue = immutable.Queue[Event]()
+    var subscribers = immutable.Set[ActorRef]()
+
+    override def receive: Receive = {
+      case event: Event =>
+        if (EventFiltering.isAllowed(event)) {
+          queue = queue.enqueueFinite(event, maxElementsInQueue)
+          subscribers.foreach(_ ! event)
+        }
+      case Subscribe =>
+        val s = sender()
+        subscribers += s
+        queue.foreach(s ! _)
+      case Unsubscribe =>
+        subscribers -= sender()
+    }
+
+  }))
+
+
+  private[akka] def publish(event: Event): Unit = {
+    publisher ! event
+  }
+
+  def subscribe(subscriber: ActorRef): Unit = {
+    publisher.tell(Subscribe, subscriber)
+  }
+
+}
