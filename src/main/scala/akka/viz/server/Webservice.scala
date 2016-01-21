4f54f0c3b491d3c93a55838840abbf14b2bca92f
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-01-21 14:03:32 +0100

    Tiny http server with websocket of passed messages

---
+++ src/main/scala/akka/viz/server/Webservice.scala
@@ -0,0 +1,71 @@
+package akka.viz.server
+
+
+import akka.actor.{ActorRef, ActorSystem}
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
+import akka.http.scaladsl.server.Directives
+import akka.stream.scaladsl._
+import akka.stream.stage._
+import akka.stream.{Materializer, OverflowStrategy}
+import akka.viz.MessageSerialization
+import akka.viz.config.Config
+import akka.viz.events.EventSystem.Subscribe
+import akka.viz.events.{Received, Event, EventSystem}
+import upickle.Js
+
+
+class Webservice(implicit fm: Materializer, system: ActorSystem) {
+
+  import Directives._
+
+  def route: Flow[HttpRequest, HttpResponse, Any] = {
+    get {
+      pathSingleSlash {
+        getFromResource("web/index.html")
+      } ~
+        path("frontend-launcher.js")(getFromResource("frontend-launcher.js")) ~
+        path("frontend-fastopt.js")(getFromResource("frontend-fastopt.js")) ~
+        path("stream") {
+          handleWebsocketMessages(tracingEventsFlow)
+
+        }
+    } ~
+      getFromResourceDirectory("web")
+  }
+
+  private implicit val actorRefWriter = upickle.default.Writer[ActorRef] {
+    case actorRef => Js.Str(actorRef.path.toSerializationFormat)
+  }
+
+  def tracingEventsFlow: Flow[Message, Message, Any] = {
+    val in = Flow[Message].to(Sink.ignore)
+    val out = Source.actorRef[Event](Config.bufferSize, OverflowStrategy.dropNew)
+      .via(eventSerialization)
+      .via(Flow[Js.Value].map {
+        i =>
+          println(i)
+          i
+      })
+      .via(jsonPrinter)
+      .map(TextMessage(_))
+      .mapMaterializedValue(EventSystem.subscribe(_))
+
+    Flow.fromSinkAndSource(in, out)
+  }
+
+  def eventSerialization: Flow[Event, Js.Value, Any] = Flow[Event].map {
+    case Received(sender, receiver, message) =>
+      Js.Obj(
+        "sender" -> Js.Str(sender.path.toSerializationFormat),
+        "receiver" -> Js.Str(receiver.path.toSerializationFormat),
+        "message" -> MessageSerialization.serialize(message)
+      )
+  }
+
+  def jsonPrinter: Flow[Js.Value, String, Any] = Flow[Js.Value].map {
+    case json =>
+      upickle.json.write(json)
+  }
+
+}
