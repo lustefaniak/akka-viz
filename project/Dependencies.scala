535b6356b8dc26412fdf7f6e799190c1f2bdad23
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-01-22 15:03:18 +0100

    Add dependencies to scala-pickling

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,7 @@ object Dependencies {
     val akkaStream = "2.0.2"
     val scalatest = "3.0.0-M15"
     val upickle = "0.3.6"
+    val pickling = "0.10.1"
   }
 
   val upickle = "com.lihaoyi" %% "upickle" % Versions.upickle
@@ -15,7 +16,8 @@ object Dependencies {
   val akkaHttp = "com.typesafe.akka" %% "akka-http-experimental" % Versions.akkaStream
   val akkaHttpTestkit = "com.typesafe.akka" %% "akka-http-testkit-experimental" % Versions.akkaStream
   val scalatest = "org.scalatest" %% "scalatest" % Versions.scalatest % "test"
+  val pickling = "org.scala-lang.modules" %% "scala-pickling" % Versions.pickling
 
-  val backend = Seq(upickle, akkaActor, akkaStream, akkaHttp, scalatest)
+  val backend = Seq(upickle, akkaActor, akkaStream, akkaHttp, scalatest, pickling)
 
 }
