8eda80f64961fc0f35b42bbdb9782056962f8d0a
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-01-26 14:52:28 +0100

    Catch exceptions when serializing messages

--- backend/src/main/scala/akka/viz/MessageSerialization.scala
+++ backend/src/main/scala/akka/viz/MessageSerialization.scala
@@ -2,11 +2,17 @@ package akka.viz
 
 import scala.pickling.Defaults._
 import scala.pickling.json._
+import scala.util.{Success, Try}
 
 object MessageSerialization {
 
   def serialize(message: Any): String = {
-    message.pickle.value
+    Try {
+      message.pickle.value
+    }.recoverWith {
+      case t: Throwable =>
+        Success(s"{'error':'Failed to serialize: ${t.getMessage}'}")
+    }.get
   }
 
 }
