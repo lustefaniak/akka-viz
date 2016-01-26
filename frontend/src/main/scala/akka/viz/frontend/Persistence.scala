5472fe161eee8a5f37f713e2200fa3764d790210
Patryk Koryzna <patryk.koryzna@blstream.com>
2016-01-26 13:25:16 +0100

    add error handling to persistent var

--- frontend/src/main/scala/akka/viz/frontend/Persistence.scala
+++ frontend/src/main/scala/akka/viz/frontend/Persistence.scala
@@ -5,10 +5,21 @@ import org.scalajs.dom.ext.LocalStorage
 import rx.Var
 import upickle.default._
 
+import scala.util.Try
+
 trait Persistence {
 
   def persistedVar[T: Writer : Reader](initialValue: T, name: String): Var[T] = {
-    val v = Var[T](LocalStorage(name).map(s => read[T](s)).getOrElse(initialValue))
+    val init: T =
+      LocalStorage(name).flatMap { stored =>
+        val unpickled: Try[T] = Try(read[T](stored))
+        unpickled.failed.foreach(thr => console.log(s"failed to unpickle $name, $thr"))
+
+        unpickled.toOption
+      }.getOrElse(initialValue)
+
+    val v = Var[T](init)
+
     val storeObs = v.triggerLater {
       LocalStorage.update(name, upickle.default.write(v.now))
       console.log(s"persisted $name=${v.now}")
