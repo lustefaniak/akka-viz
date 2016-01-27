abae92f3e4f560608e1af5ca31164ba68fa96542
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-01-28 00:21:27 +0100

    Add demo of dining hakkers from akka-samples

--- backend/src/main/scala/postoffice/postoffice.scala
+++ backend/src/main/scala/postoffice/postoffice.scala
@@ -6,60 +6,48 @@ import akka.actor.{Actor, ActorSystem, Props}
 
 import scala.util.Random
 
-object PostOfficeExample extends App {
-
-  val system = ActorSystem("post-office")
-
-  val lazyActorProps = Props(new Actor {
-    override def receive: Receive = {
-      case msg =>
-        Thread.sleep(Random.nextInt(2000))
-        sender() ! msg
-    }
-  })
-
-  val lazyActor1 = system.actorOf(lazyActorProps, "lazy1")
-  val lazyActor2 = system.actorOf(lazyActorProps, "lazy2")
-  for (i <- 0 to 1000) {
-    lazyActor1.tell("doit", lazyActor2)
-  }
-
-  for (city <- PostOffice.Cities) {
-    system.actorOf(Props(classOf[PostOfficeActor], PostOffice(city)), city.toString)
-  }
-
-}
-
 case class Parcel(source: City, destination: City, weight: Weight)
+
 case class PostOffice(city: City)
 
 sealed trait ParcelAction
+
 case class Pickup(date: LocalDateTime, parcel: Parcel) extends ParcelAction
+
 case class Delivery(date: LocalDateTime, parcel: Parcel) extends ParcelAction
+
 case class Rejected(date: LocalDateTime, parcel: Parcel) extends ParcelAction
 
 sealed trait City
+
 case object Wroclaw extends City
+
 case object Lodz extends City
+
 case object Warszawa extends City
+
 case object Poznan extends City
+
 case object Krakow extends City
+
 case object GorzowWlkp extends City
+
 case object Berlin extends City
 
 object PostOffice {
   val ParcelRoutes = Map[(City, City), List[City]](
-    (Wroclaw -> Warszawa) ->   (Wroclaw :: Lodz :: Warszawa :: Nil),
-    (Krakow -> Warszawa) ->    (Krakow :: Lodz :: Warszawa :: Nil),
-    (Berlin -> Warszawa) ->    (Berlin :: GorzowWlkp :: Poznan :: Warszawa :: Nil),
+    (Wroclaw -> Warszawa) -> (Wroclaw :: Lodz :: Warszawa :: Nil),
+    (Krakow -> Warszawa) -> (Krakow :: Lodz :: Warszawa :: Nil),
+    (Berlin -> Warszawa) -> (Berlin :: GorzowWlkp :: Poznan :: Warszawa :: Nil),
     (GorzowWlkp -> Wroclaw) -> (GorzowWlkp :: Poznan :: Wroclaw :: Nil)
   )
 
-  def route: ((City, City)) => List[City] = { case (src, dest) =>
-    ParcelRoutes
-      .get(src -> dest)
-      .orElse(ParcelRoutes.get(dest -> src).map(_.reverse))
-      .getOrElse(src :: dest :: Nil)
+  def route: ((City, City)) => List[City] = {
+    case (src, dest) =>
+      ParcelRoutes
+        .get(src -> dest)
+        .orElse(ParcelRoutes.get(dest -> src).map(_.reverse))
+        .getOrElse(src :: dest :: Nil)
   }
 
   val WeightLimit = 5.00
@@ -75,4 +63,12 @@ object PostOffice {
     Krakow,
     GorzowWlkp,
     Berlin)
+
+
+  def run(implicit system: ActorSystem) = {
+    for (city <- PostOffice.Cities) {
+      system.actorOf(Props(classOf[PostOfficeActor], PostOffice(city)), city.toString)
+    }
+  }
+
 }
