59b43aaddaadb692e868f6ef83f82c7cb2ae2672
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-02-03 15:28:41 +0100

    Simple serialization infrastrucutre

---
+++ backend/src/main/scala/akka/viz/serialization/DefaultSerializers.scala
@@ -0,0 +1,36 @@
+package akka.viz.serialization
+
+import upickle.Js
+import upickle.Js.Value
+
+import scala.collection.mutable
+import scala.reflect.ClassTag
+
+object DefaultSerializers {
+
+  private implicit def fnToAkkaVizSerializer[T: ClassTag](fn: (T) => Js.Value) = {
+    new AkkaVizSerializer {
+      override def serialize(obj: Any): Value = fn(obj.asInstanceOf[T])
+
+      override def canSerialize(obj: Any): Boolean = obj match {
+        case t: T => true
+        case _ => false
+      }
+    }
+  }
+
+  def mappers: mutable.Map[Class[_], AkkaVizSerializer] = mutable.Map(
+    classOf[String] -> ((s: String) => Js.Str(s)),
+    classOf[Int] -> ((i: Int) => Js.Num(i)),
+    classOf[java.lang.Integer] -> ((i: java.lang.Integer) => Js.Num(i.toInt)),
+    classOf[Long] -> ((i: Long) => Js.Num(i)),
+    classOf[java.lang.Long] -> ((i: java.lang.Long) => Js.Num(i.toLong)),
+    classOf[Double] -> ((i: Double) => Js.Num(i)),
+    classOf[java.lang.Double] -> ((i: java.lang.Double) => Js.Num(i)),
+    classOf[Float] -> ((i: Float) => Js.Num(i)),
+    classOf[java.lang.Float] -> ((i: java.lang.Float) => Js.Num(i.toFloat)),
+    classOf[Boolean] -> ((b: Boolean) => if (b) Js.True else Js.False),
+    classOf[java.lang.Boolean] -> ((b: java.lang.Boolean) => if (b) Js.True else Js.False)
+  )
+
+}
--- backend/src/main/scala/akka/viz/serialization/MessageSerialization.scala
+++ backend/src/main/scala/akka/viz/serialization/MessageSerialization.scala
@@ -1,14 +1,67 @@
 package akka.viz.serialization
 
-import scala.pickling.Defaults._
-import scala.pickling.json._
+import org.clapper.classutil.ClassFinder
+import upickle.Js
+import upickle.json.FastRenderer
+
+import scala.collection.mutable
 import scala.util.{Success, Try}
+
+trait AkkaVizSerializer {
+  def canSerialize(obj: Any): Boolean
+
+  def serialize(obj: Any): Js.Value
+}
 
 object MessageSerialization {
 
+  private val placeholderSerializer = new AkkaVizSerializer {
+    override def serialize(obj: Any): Js.Value = Js.Obj(
+      "$type" -> Js.Str(obj.getClass.getCanonicalName)
+    )
+
+    override def canSerialize(obj: Any): Boolean = true
+  }
+
+  private val rm = scala.reflect.runtime.currentMirror
+
+  private val serializers: List[AkkaVizSerializer] = {
+    val finder = ClassFinder()
+    val classes = finder.getClasses.filter(_.isConcrete).filter(_.implements("akka.viz.serialization.AkkaVizSerializer"))
+
+    classes.flatMap {
+      cls =>
+        val clazz = Class.forName(cls.name)
+        val classSymbol = rm.classSymbol(clazz)
+        if (classSymbol.isModule) {
+          Some(rm.reflectModule(classSymbol.asModule).instance.asInstanceOf[AkkaVizSerializer])
+        } else {
+          val constructors = classSymbol.toType.members.filter(_.isConstructor).map(_.asMethod)
+          val constructor = constructors.filter(_.isPublic).filter(_.paramLists.exists(_.size == 0)).headOption
+          constructor.map {
+            constructor =>
+              rm.reflectClass(classSymbol).reflectConstructor(constructor).apply().asInstanceOf[AkkaVizSerializer]
+          }
+        }
+    }.toList
+  }
+
+  private val mappers = DefaultSerializers.mappers
+
+  private def findSerializerForObject(obj: Any): AkkaVizSerializer = {
+    serializers.find {
+      _.canSerialize(obj)
+    }.getOrElse(placeholderSerializer)
+  }
+
+  def getSerializerFor(obj: Any): AkkaVizSerializer = {
+    mappers.getOrElseUpdate(obj.getClass, findSerializerForObject(obj))
+  }
+
   def serialize(message: Any): String = {
     Try {
-      message.pickle.value
+      val serialized = getSerializerFor(message).serialize(message)
+      FastRenderer.render(serialized)
     }.recoverWith {
       case t: Throwable =>
         println(s"Unable to serialize '${message}'")
