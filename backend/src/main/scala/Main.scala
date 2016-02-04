aa2d9d39f8816cb6cf5e9a8ca4500204794815f7
Jakub Jagusztyn <k.jagusztyn@gmail.com>
2016-02-04 11:14:12 +0100

    Run Russian roulette demo

--- backend/src/main/scala/Main.scala
+++ backend/src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import akka.actor.{Actor, ActorSystem, Props}
 import fsm.DiningHakkersOnFsm
 import postoffice.PostOffice
+import roulette.Game
 import spray.SprayDemo
 
 import scala.util.Random
@@ -9,6 +10,7 @@ object Main extends App {
   DiningHakkersOnFsm.run(ActorSystem("fsm"))
   PostOffice.run(ActorSystem("post-office"))
   SprayDemo.run(ActorSystem("spray"))
+  new Game(5).run(ActorSystem("russian-roulette"))
 
   val system = ActorSystem("small-demos")
   val lazyActorProps = Props(new Actor {
