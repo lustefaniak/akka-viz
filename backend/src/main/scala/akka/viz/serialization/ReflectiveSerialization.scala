0840bc30a15d500c534ad49396aa62f3f15da048
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-02-05 15:33:42 +0100

    Split ReflectiveSerialization into two methods for easier reading

--- backend/src/main/scala/akka/viz/serialization/ReflectiveSerialization.scala
+++ backend/src/main/scala/akka/viz/serialization/ReflectiveSerialization.scala
@@ -9,35 +9,44 @@ trait ReflectiveSerialization {
 
   private val shouldInspectObjects = Config.inspectObjects
 
-  def fieldSelector(inspector: ClassInspector): Set[String] = {
+  @inline
+  protected def fieldSelector(inspector: ClassInspector): Set[String] = {
     inspector.allFieldNames
   }
 
+  @inline
   def reflectiveSerialize(obj: Any, context: SerializationContext): Js.Value = {
     if (context.depth() <= maxSerializationDepth) {
-      val inspector = CachingClassInspector.of(obj.getClass)
-      if (!inspector.isObject || shouldInspectObjects) {
-        val fields = inspector.inspect(obj, fieldSelector(inspector))
-        Js.Obj(
-          Seq("$type" -> Js.Str(obj.getClass.getName))
-            ++ fields.toSeq.map {
-              case (fieldName, rawValue) =>
-                fieldName -> MessageSerialization.serialize(rawValue, context)
-            }: _*
-        )
-      } else {
-        shallowModuleSerialization(obj)
-      }
+      inspect(obj, context)
     } else {
       maxDepthReached
     }
   }
 
-  protected def maxDepthReached: Js.Value = {
+  @inline
+  private def inspect(obj: Any, context: SerializationContext): Js.Value = {
+    val inspector = CachingClassInspector.of(obj.getClass)
+    if (!inspector.isObject || shouldInspectObjects) {
+      val fields = inspector.inspect(obj, fieldSelector(inspector))
+      Js.Obj(
+        Seq("$type" -> Js.Str(obj.getClass.getName))
+          ++ fields.toSeq.map {
+            case (fieldName, rawValue) =>
+              fieldName -> MessageSerialization.serialize(rawValue, context)
+          }: _*
+      )
+    } else {
+      shallowModuleSerialization(obj)
+    }
+  }
+
+  @inline
+  private def maxDepthReached: Js.Value = {
     Js.Obj("$error" -> Js.Str(s"Max serialization depth of ${maxSerializationDepth} reached"))
   }
 
-  protected def shallowModuleSerialization(module: Any): Js.Value = {
+  @inline
+  private def shallowModuleSerialization(module: Any): Js.Value = {
     Js.Obj("$object" -> Js.Str(module.getClass.getName.stripSuffix("$")))
   }
 
