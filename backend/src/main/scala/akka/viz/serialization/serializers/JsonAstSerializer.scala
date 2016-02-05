6fccec63d18f5029d9b16946a10fda136a5ddd44
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-02-05 13:45:08 +0100

    Serializer for Js.Value

---
+++ backend/src/main/scala/akka/viz/serialization/serializers/JsonAstSerializer.scala
@@ -0,0 +1,18 @@
+package akka.viz.serialization.serializers
+
+import akka.viz.serialization.AkkaVizSerializer
+import upickle.Js
+
+case object JsonAstSerializer extends AkkaVizSerializer {
+  override def serialize(obj: Any): Js.Value = {
+    obj match {
+      case a: Js.Value => a
+    }
+  }
+
+  override def canSerialize(obj: Any): Boolean = obj match {
+    case t: Js.Value => true
+    case _ => false
+  }
+}
+
