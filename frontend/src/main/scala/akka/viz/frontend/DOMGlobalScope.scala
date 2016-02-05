1207ef0a2e789baa33372ebe05c76204c49219f4
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-02-05 20:49:11 +0100

    Ensure DOMGlobalScope members are lazy

--- frontend/src/main/scala/akka/viz/frontend/DOMGlobalScope.scala
+++ frontend/src/main/scala/akka/viz/frontend/DOMGlobalScope.scala
@@ -4,7 +4,7 @@ import scala.scalajs.js
 
 @js.native
 object DOMGlobalScope extends js.GlobalScope {
-  val graph: js.Dynamic = js.native
-  val $: js.Dynamic = js.native
+  def graph: js.Dynamic = js.native
+  def $: js.Dynamic = js.native
   def grayOut: js.Dynamic = js.native
 }
