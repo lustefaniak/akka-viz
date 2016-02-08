84a86a499db7af6cda3b00a3a7231eca5e431b00
Patryk Koryzna <patryk.koryzna@blstream.com>
2016-02-08 10:37:05 +0100

    add ReplayableEvent trait marking important events for new clients

--- backend/src/main/scala/akka/viz/events/types/package.scala
+++ backend/src/main/scala/akka/viz/events/types/package.scala
@@ -10,15 +10,17 @@ package object types {
 
   sealed trait BackendEvent
 
+  sealed trait ReplayableEvent
+
   case class Received(sender: ActorRef, receiver: ActorRef, message: Any) extends InternalEvent
 
   case class ReceivedWithId(eventId: Long, sender: ActorRef, receiver: ActorRef, message: Any) extends BackendEvent
 
-  case class Spawned(ref: ActorRef, parent: ActorRef) extends InternalEvent with BackendEvent
+  case class Spawned(ref: ActorRef, parent: ActorRef) extends InternalEvent with BackendEvent with ReplayableEvent
 
   case class MailboxStatus(owner: ActorRef, size: Int) extends InternalEvent with BackendEvent
 
-  case class Instantiated(actorRef: ActorRef, actor: Actor) extends InternalEvent with BackendEvent
+  case class Instantiated(actorRef: ActorRef, actor: Actor) extends InternalEvent with BackendEvent with ReplayableEvent
 
   case class AvailableMessageTypes(classes: List[Class[_ <: Any]]) extends BackendEvent
 
@@ -32,8 +34,8 @@ package object types {
 
   case class CurrentActorState(actorRef: ActorRef, actor: Actor) extends InternalEvent with BackendEvent
 
-  case class ReceiveDelaySet(duration: Duration) extends InternalEvent with BackendEvent
+  case class ReceiveDelaySet(duration: Duration) extends InternalEvent with BackendEvent with ReplayableEvent
 
-  case class Killed(actorRef: ActorRef) extends InternalEvent with BackendEvent
+  case class Killed(actorRef: ActorRef) extends InternalEvent with BackendEvent with ReplayableEvent
 
 }
