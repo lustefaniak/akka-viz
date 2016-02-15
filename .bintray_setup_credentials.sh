467b1f85f6827dcbae95ee3c72f5c35f7e79ccd0
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-02-15 23:39:40 +0100

    Fix appending user and password to bintray credentials file

--- .bintray_setup_credentials.sh
+++ .bintray_setup_credentials.sh
@@ -1,2 +1,4 @@
+echo "realm = Bintray API Realm" > .bintray_credentials
+echo "host = api.bintray.com" >> .bintray_credentials
 echo "user = ${BINTRAY_USER}" >> .bintray_credentials
 echo "password = ${BINTRAY_TOKEN}" >> .bintray_credentials
