f45b51e97c884ee96480df529a3b24bd18eac096
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-02-23 18:44:16 +0100

    Helper methods to decode and encode js.ArrayBuffer <=> String

--- frontend/src/main/scala/akkaviz/frontend/DOMGlobalScope.scala
+++ frontend/src/main/scala/akkaviz/frontend/DOMGlobalScope.scala
@@ -1,6 +1,7 @@
 package akkaviz.frontend
 
 import scala.scalajs.js
+import scala.scalajs.js.typedarray.ArrayBuffer
 
 @js.native
 object DOMGlobalScope extends js.GlobalScope {
@@ -9,4 +10,9 @@ object DOMGlobalScope extends js.GlobalScope {
   def $: js.Dynamic = js.native
 
   def grayOut: js.Dynamic = js.native
+
+  def ab2str(ab: ArrayBuffer): String = js.native
+
+  def str2ab(str: String): ArrayBuffer = js.native
+
 }
