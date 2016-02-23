ce234d61f9e3d1e6939aa435fa67ee77556c4a62
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-02-23 19:37:14 +0100

    Reformat old code using Scalariform

--- frontend/src/main/scala/akkaviz/frontend/components/UnconnectedOnOff.scala
+++ frontend/src/main/scala/akkaviz/frontend/components/UnconnectedOnOff.scala
@@ -4,7 +4,6 @@ import org.scalajs.dom.Event
 import org.scalajs.dom.html.Element
 import rx.Var
 
-
 class UnconnectedOnOff(status: Var[Boolean]) extends OnOffWithLabel with Component {
   lbl.innerHTML = "Show actors without any connections on graph"
   inp.onchange = { e: Event =>
