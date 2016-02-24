f66dd633b6fab08cb1e1f5f04058101631b8cd6d
Jakub Jagusztyn <k.jagusztyn@gmail.com>
2016-02-24 15:42:49 +0100

    Publish ask-related events

--- monitoring/src/main/scala/akka/viz/AskInstrumentation.scala
+++ monitoring/src/main/scala/akka/viz/AskInstrumentation.scala
@@ -1,15 +1,21 @@
 package akka.viz
 
+import java.util.concurrent.atomic.AtomicLong
+
 import akka.actor.ActorRef
 import akka.util.Timeout
 import akkaviz.config.Config
+import akkaviz.events.EventSystem
+import akkaviz.events.types.{Answer, AnswerFailed, Question}
 import org.aspectj.lang.annotation._
 
-import scala.concurrent.{Future, Promise}
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 @Aspect
 class AskInstrumentation {
 
+  private val askCounter: AtomicLong = new AtomicLong(0)
   private val internalSystemName = Config.internalSystemName
 
   @Pointcut("execution (* akka.pattern..*.internalAsk$extension(..)) && args(recipient, msg, timeout, sender)")
@@ -25,29 +31,27 @@ class AskInstrumentation {
                        timeout: Timeout,
                        sender: ActorRef): Unit = {
     if (!isSystemActor(recipient)) {
-      publishQuestion(Option(sender), recipient, msg)
-      scheduleAnswer(future)
+      val questionId = askCounter.incrementAndGet()
+      publishQuestion(questionId, Option(sender), recipient, msg)
+      scheduleAnswer(questionId, future)
     }
   }
 
   private def isSystemActor(ref: ActorRef) =
     ref.path.address.system == internalSystemName
 
-  def publishQuestion(from: Option[ActorRef], to: ActorRef, msg: Any) = {
-    println("~~~~~~~~ ASK ~~~~~~~~")
-    println("FROM: " + from.map(_.path.toSerializationFormat).getOrElse("noSender"))
-    println("TO: " + to.path.toSerializationFormat)
-    println("MSG: " + msg)
-    println("~~~~~~~~~~~~~~~~~~~~~")
+  def publishQuestion(questionId: Long,
+                      from: Option[ActorRef],
+                      to: ActorRef,
+                      msg: Any): Unit = {
+    EventSystem.report(Question(questionId, from, to, msg))
   }
 
-  def scheduleAnswer(answerFuture: Future[Any]) = {
+  def scheduleAnswer(questionId: Long, answerFuture: Future[Any]): Unit = {
     implicit val ec = scala.concurrent.ExecutionContext.global
-    answerFuture.onSuccess { case msg =>
-      println("REPLIED: " + msg)
-    }
-    answerFuture.onFailure { case ex =>
-      println("EXCEPTION THROWN: " + ex)
+    answerFuture.onComplete {
+      case Success(msg) => EventSystem.report(Answer(questionId, msg))
+      case Failure(ex) => EventSystem.report(AnswerFailed(questionId, ex))
     }
   }
 }
