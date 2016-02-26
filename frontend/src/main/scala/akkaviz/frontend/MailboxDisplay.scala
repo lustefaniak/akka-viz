5b6d4036eee72b02a895ec469d445a3e2cd6ca11
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-02-26 17:16:31 +0100

    Use GraphView in MailboxDisplay

--- frontend/src/main/scala/akkaviz/frontend/MailboxDisplay.scala
+++ frontend/src/main/scala/akkaviz/frontend/MailboxDisplay.scala
@@ -1,14 +1,12 @@
 package akkaviz.frontend
 
+import akkaviz.frontend.components.GraphView
 import akkaviz.protocol
 
 import scala.scalajs.js
 
 trait MailboxDisplay {
-  private val graph = DOMGlobalScope.graph
-
-  def handleMailboxStatus(mb: protocol.MailboxStatus): Unit = {
-    // todo: add a actor metadata type when we'll display more info on the graph?
-    graph.addNode(mb.owner, js.Dictionary("mailboxSize" -> mb.size))
+  def handleMailboxStatus(mb: protocol.MailboxStatus, graphView: GraphView): Unit = {
+    graphView.ensureNodeExists(mb.owner, FrontendUtil.shortActorName(mb.owner), js.Dictionary("mailboxSize" -> mb.size))
   }
 }
