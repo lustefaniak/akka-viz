14c95e684e3429c2a310bee197f9100c63e72e42
Patryk Koryzna <patryk.koryzna@blstream.com>
2016-02-26 08:43:24 +0100

    produce zero for actors that have been measured previously but didn't receive any messages during period (cherry picked from commit f83af6d)

--- monitoring/src/main/scala/akkaviz/events/ThroughputMeasurementFlow.scala
+++ monitoring/src/main/scala/akkaviz/events/ThroughputMeasurementFlow.scala
@@ -1,5 +1,6 @@
 package akkaviz.events
 
+import akka.actor.ActorRef
 import akka.stream.scaladsl.Flow
 import akkaviz.events.types.{ThroughputMeasurement, ReceivedWithId, BackendEvent}
 
@@ -12,7 +13,12 @@ object ThroughputMeasurementFlow {
       .groupedWithin(Int.MaxValue, period)
       .map { refs =>
         refs.groupBy(identity).mapValues(_.length)
-      }.mapConcat { m =>
+      }
+      .scan(Map[ActorRef, Int]()) { case (previous, current) =>
+        // produce zero for actors that have been measured previously but didn't receive any messages during `period`
+        current ++ (for { k <- current.keySet.diff(previous.keySet) } yield k -> 0)
+      }
+      .mapConcat { m =>
         for {
           (ref, count) <- m
         } yield ThroughputMeasurement(ref, count / (period.toMillis.toDouble / 1.second.toMillis.toDouble))
