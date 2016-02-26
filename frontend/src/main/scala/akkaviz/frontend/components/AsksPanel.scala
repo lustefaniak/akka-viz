c0c2b0754099b0e2d9fced22602dc3e63dc79631
Jakub Jagusztyn <k.jagusztyn@gmail.com>
2016-02-26 16:28:18 +0100

    Display ask messages and their responses(or exceptions)

--- frontend/src/main/scala/akkaviz/frontend/components/AsksPanel.scala
+++ frontend/src/main/scala/akkaviz/frontend/components/AsksPanel.scala
@@ -1,16 +1,16 @@
 package akkaviz.frontend.components
 
-import akkaviz.protocol.{AnswerFailed, Answer, Question}
+import akkaviz.frontend.DOMGlobalScope._
+import akkaviz.frontend.PrettyJson
+import akkaviz.protocol.{AskResult, AnswerFailed, Answer, Question}
 import rx.Var
 
 import scala.collection.mutable
 import scalatags.JsDom.all._
 
+class AsksPanel(selectedActors: Var[Set[String]]) extends Component with PrettyJson {
 
-class AsksPanel(selectedActors: Var[Set[String]]) extends Component {
-
-  private val asks: mutable.Map[Question, Option[Either[Answer, AnswerFailed]]] = mutable.Map()
-
+  private val asks: mutable.Set[Question] = mutable.Set()
 
   override def render = panelBody.render
 
@@ -33,48 +33,89 @@ class AsksPanel(selectedActors: Var[Set[String]]) extends Component {
   lazy val tableBody = tbody().render
 
   def receivedQuestion(q: Question): Unit = {
-    asks += (q -> None)
+    asks += q
     tableBody.appendChild(tableRowAwaiting(q).render)
   }
 
   def receivedAnswer(a: Answer): Unit = {
-    val question = asks.keysIterator.find(_.id == a.questionId)
+    val question = asks.find(_.id == a.questionId)
     if (question.isDefined) {
       val oldRow = tableBody.querySelector(s"#question-${a.questionId}")
       tableBody.replaceChild(tableRowAnswered(question.get, a).render, oldRow)
     }
   }
 
-  def receivedAnswerFailed(a: AnswerFailed): Unit = {
-    val question = asks.keysIterator.find(_.id == a.questionId)
+  def receivedAnswerFailed(af: AnswerFailed): Unit = {
+    val question = asks.find(_.id == af.questionId)
     if (question.isDefined) {
-      val oldRow = tableBody.querySelector(s"#question-${a.questionId}")
-      tableBody.replaceChild(tableRowAnswerFailed(question.get, a).render, oldRow)
+      val oldRow = tableBody.querySelector(s"#question-${af.questionId}")
+      tableBody.replaceChild(tableRowAnswerFailed(question.get, af).render, oldRow)
     }
   }
 
   private def tableRowAwaiting(question: Question) =
-    tr(
-      id := s"question-${question.id}",
-      td(question.sender.getOrElse("No sender"): String),
-      td(question.actorRef),
-      td("Awaiting answer")
-    )
+    tableRow(question, None)
 
   private def tableRowAnswered(question: Question, answer: Answer) =
-    tr(
-      id := s"question-${question.id}",
-      td(question.sender.getOrElse("No sender"): String),
-      td(question.actorRef),
-      td("Answered")
-    )
+    tableRow(question, Some(answer))
 
   private def tableRowAnswerFailed(question: Question, ansFailed: AnswerFailed) =
+    tableRow(question, Some(ansFailed))
+
+  private def tableRow(question: Question, result: Option[AskResult]) = {
+    val details = detailsView(question, result)
+    val status = statusText(result)
     tr(
+      "data-toggle".attr := "modal",
+      "data-target".attr := "#ask-modal",
       id := s"question-${question.id}",
       td(question.sender.getOrElse("No sender"): String),
       td(question.actorRef),
-      td("Answer failed")
+      td(status),
+      onclick := { () =>
+        $("#ask-modal-body").html(details.render)
+      }
     )
+  }
+
+  def detailsView(question: Question, result: Option[AskResult]) = result match {
+    case None => unansweredQuestionDetails(question)
+    case Some(ans: Answer) => answeredQuestionDetails(question, ans)
+    case Some(ansFailed: AnswerFailed) => failedAnswerDetails(question, ansFailed)
+  }
+
+  def statusText(result: Option[AskResult]) = result match {
+    case None => "Waiting for answer"
+    case Some(ans: Answer) => "Answered"
+    case Some(ansFailed: AnswerFailed) => "Failed to answer"
+  }
+
+  def unansweredQuestionDetails(q: Question) =
+    div(
+      questionHeader(q),
+      pre(prettyPrintJson(q.message)),
+      h4(s"Waiting for answer from ${q.actorRef}")
+    )
+
+  def answeredQuestionDetails(q: Question, a: Answer) =
+    div(
+      questionHeader(q),
+      pre(prettyPrintJson(q.message)),
+      h3("Answer from " + q.actorRef),
+      pre(prettyPrintJson(a.message))
+    )
+
+  def failedAnswerDetails(q: Question, af: AnswerFailed) =
+    div(
+      questionHeader(q),
+      pre(prettyPrintJson(q.message)),
+      h3("Exception"),
+      pre(af.ex)
+    )
+
+  def questionHeader(q: Question) = q.sender match {
+    case Some(s) => h3(s"Question from $s")
+    case None => h3("Question(no sender)")
+  }
 
 }
