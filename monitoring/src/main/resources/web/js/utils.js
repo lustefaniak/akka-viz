a850dc4888aa8c485f4c7bdc86337944832b5bd5
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-03-02 23:08:12 +0100

    Remove vivagraph and use visjs

---
+++ monitoring/src/main/resources/web/js/utils.js
@@ -0,0 +1,13 @@
+
+function ab2str(buf) {
+    return String.fromCharCode.apply(null, new Uint8Array(buf));
+}
+
+function str2ab(str) {
+    var buf = new ArrayBuffer(str.length); // 2 bytes for each char
+    var bufView = new Uint8Array(buf);
+    for (var i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
