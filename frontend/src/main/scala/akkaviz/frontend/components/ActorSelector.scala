9f4340b8948342956e77c40221b2a2598827101e
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-03-03 23:32:34 +0100

    Display actor details in modal dialog

--- frontend/src/main/scala/akkaviz/frontend/components/ActorSelector.scala
+++ frontend/src/main/scala/akkaviz/frontend/components/ActorSelector.scala
@@ -6,7 +6,7 @@ import akkaviz.frontend.FrontendUtil.actorComponent
 import akkaviz.frontend.{DOMGlobalScope, PrettyJson}
 import akkaviz.protocol.ActorFailure
 import org.scalajs.dom.html._
-import org.scalajs.dom.{Element => domElement, Node, console}
+import org.scalajs.dom.{Element => domElement, document, Node, console}
 import rx.{Rx, Var}
 
 import scala.scalajs.js
@@ -21,46 +21,67 @@ class ActorSelector(
     actorFailures: Var[Seq[ActorFailure]]
 ) extends PrettyJson with Component {
 
-  val popoverContent: ThisFunction0[domElement, Node] = (that: domElement) => {
+  def attach(parent: domElement): Unit = {
+    val elem = div(cls := "panel-body", id := "actortree",
+      table(
+        cls := "table table-striped table-hover",
+        thead(
+          tr(th(), th("Actor", p(
+            float.right,
+            input(id := "actorfilter-regex", size := 12, tpe := "text", placeholder := "Filter...", marginRight := 1.em, onkeyup := regexActorFilter),
+            a(href := "#", id := "actorfilter-select-all", "all", onclick := selectAllActorFilters),
+            " | ",
+            a(href := "#", id := "actorfilter-select-none", "none", onclick := clearActorFilters)
+          )))
+        ),
+        actorTreeTbody
+      )).render
+    parent.appendChild(elem)
+  }
+
+  private[this] val popoverContent: ThisFunction0[domElement, Node] = (that: domElement) => {
     val actor: String = that.getAttribute("data-actor")
     val content = div().render
     val stateVar = currentActorState(actor)
-    stateVar.trigger {
-      val state = stateVar.now
-      val renderedState = Seq[Frag](
-        div(strong("Class: "), state.className.getOrElse[String]("Unknown class")),
-        div(strong("Is dead: "), state.isDead.toString),
-        div(strong("Internal state: "), pre(state.internalState.map(prettyPrintJson).getOrElse[String]("Internal state unknown"))),
-        div(strong("Is FSM: "), state.fsmState.isDefined.toString),
-        state.fsmState.map[Frag] {
-          fsm =>
-            Seq(
-              div(strong("FSM State: "), pre(prettyPrintJson(fsm.currentState))),
-              div(strong("FSM Data: "), pre(prettyPrintJson(fsm.currentData)))
-            )
-        }.getOrElse(()),
-        div(strong("Mailbox size: "), state.mailboxSize.map(_.toString).getOrElse[String]("Unknown")),
-        div(strong("Last updated: "), state.lastUpdatedAt.toISOString())
-      ).render
-
-      content.innerHTML = ""
-      content.appendChild(renderedState)
-    }
-
+    renderActorState(content, stateVar)
     Seq[Frag](
       h5(actor),
       content
     ).render
   }
 
-  val popoverOptions = js.Dictionary(
+  private[this] def renderActorState(element: domElement, stateVar: Var[ActorState]): Unit = {
+    stateVar.foreach {
+      state =>
+        val renderedState = Seq[Frag](
+          div(strong("Class: "), state.className.getOrElse[String]("Unknown class")),
+          div(strong("Is dead: "), state.isDead.toString),
+          div(strong("Internal state: "), pre(state.internalState.map(prettyPrintJson).getOrElse[String]("Internal state unknown"))),
+          div(strong("Is FSM: "), state.fsmState.isDefined.toString),
+          state.fsmState.map[Frag] {
+            fsm =>
+              Seq(
+                div(strong("FSM State: "), pre(prettyPrintJson(fsm.currentState))),
+                div(strong("FSM Data: "), pre(prettyPrintJson(fsm.currentData)))
+              )
+          }.getOrElse(()),
+          div(strong("Mailbox size: "), state.mailboxSize.map(_.toString).getOrElse[String]("Unknown")),
+          div(strong("Last updated: "), state.lastUpdatedAt.toISOString())
+        ).render
+
+        element.innerHTML = ""
+        element.appendChild(renderedState)
+    }
+  }
+
+  private[this] val popoverOptions = js.Dictionary(
     "content" -> popoverContent,
     "trigger" -> "hover",
     "placement" -> "right",
     "html" -> true
   )
 
-  def failureTable(failures: Seq[ActorFailure]) =
+  private[this] def failureTable(failures: Seq[ActorFailure]) =
     table(
       id := "failures-table",
       `class` := "table",
@@ -73,19 +94,37 @@ class ActorSelector(
       )
     ).render
 
-  def exceptionsButton(actorName: String, failures: Seq[ActorFailure]) =
+  private[this] def exceptionsButton(actorName: String, failures: Seq[ActorFailure]) =
     span(
       style := "color: red",
       `class` := "glyphicon glyphicon-exclamation-sign",
       "data-toggle".attr := "modal",
       "data-target".attr := "#failures-modal",
       onclick := { () =>
-        $("#actor-name").html(actorName)
-        $("#actor-failures").html(failureTable(failures))
+        document.getElementById("actor-name").innerHTML = actorName
+        document.getElementById("actor-failures").innerHTML = ""
+        document.getElementById("actor-failures").appendChild(failureTable(failures))
       }
     )
 
-  def actorExceptionsIndicator(actorName: String, failures: Seq[ActorFailure]): _root_.scalatags.JsDom.Modifier =
+  private[this] def detailsButton(actorName: String) =
+    span(
+      `class` := "glyphicon glyphicon-info-sign",
+      "data-toggle".attr := "modal",
+      "data-target".attr := "#details-modal",
+      onclick := { () =>
+        val details = div().render
+        val optionalFsm = div().render
+        val parent = document.getElementById("details-modal-body")
+        val stateVar = currentActorState(actorName)
+        renderActorState(details, stateVar)
+        parent.innerHTML = ""
+        parent.appendChild(details)
+        parent.appendChild(optionalFsm)
+      }
+    )
+
+  private[this] def actorExceptionsIndicator(actorName: String, failures: Seq[ActorFailure]): _root_.scalatags.JsDom.Modifier =
     if (failures.isEmpty) ""
     else span(b(s"${failures.length} "), exceptionsButton(actorName, failures))
 
@@ -105,7 +144,7 @@ class ActorSelector(
             })),
           td(
             actorComponent(actorName),
-            span(float.right, actorExceptionsIndicator(actorName, actorFailures.now.filter(_.actorRef == actorName)))
+            span(float.right, actorExceptionsIndicator(actorName, actorFailures.now.filter(_.actorRef == actorName)), detailsButton(actorName))
           )
         )(data("actor") := actorName).render
 
@@ -127,38 +166,21 @@ class ActorSelector(
     }
   }
 
-  lazy val actorTreeTbody = tbody().render
+  private[this] lazy val actorTreeTbody = tbody().render
 
-  def clearActorFilters: ThisFunction0[domElement, Unit] = { self: domElement =>
+  private[this] def clearActorFilters: ThisFunction0[domElement, Unit] = { self: domElement =>
     selectedActors() = Set.empty
   }
 
-  def selectAllActorFilters: ThisFunction0[domElement, Unit] = { self: domElement =>
+  private[this] def selectAllActorFilters: ThisFunction0[domElement, Unit] = { self: domElement =>
     selectedActors() = seenActors.now
   }
 
-  def regexActorFilter: ThisFunction0[domElement, Unit] = { self: domElement =>
+  private[this] def regexActorFilter: ThisFunction0[domElement, Unit] = { self: domElement =>
     val input = self.asInstanceOf[Input].value
     Try(input.r).foreach { r =>
       selectedActors() = seenActors.now.filter(_.matches(r.regex))
     }
   }
 
-  def attach(parent: domElement): Unit = {
-    val elem = div(cls := "panel-body", id := "actortree",
-      table(
-        cls := "table table-striped table-hover",
-        thead(
-          tr(th(), th("Actor", p(
-            float.right,
-            input(id := "actorfilter-regex", size := 12, tpe := "text", placeholder := "Filter...", marginRight := 1.em, onkeyup := regexActorFilter),
-            a(href := "#", id := "actorfilter-select-all", "all", onclick := selectAllActorFilters),
-            " | ",
-            a(href := "#", id := "actorfilter-select-none", "none", onclick := clearActorFilters)
-          )))
-        ),
-        actorTreeTbody
-      )).render
-    parent.appendChild(elem)
-  }
 }
