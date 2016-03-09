358d2ccc149c6b9dfad1f50d76a6f28d62d695c8
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-03-09 18:49:07 +0100

    Reduce code duplication in Alert

--- frontend/src/main/scala/akkaviz/frontend/components/Alert.scala
+++ frontend/src/main/scala/akkaviz/frontend/components/Alert.scala
@@ -8,7 +8,7 @@ import scala.scalajs.js.timers
 import scalatags.JsDom.all._
 
 class Alert extends Component {
-  lazy val connectionAlert = div(
+  private[this] lazy val connectionAlert = div(
     "Connecting...",
     cls := "alert fade in",
     id := "connectionStatus",
@@ -19,31 +19,26 @@ class Alert extends Component {
     parent.appendChild(connectionAlert)
   }
 
-  def success(msg: String, fn: MouseEvent => Unit = _ => {}) = {
+  private[this] def makeAlert(msg: String, currentFlag: String, fn: MouseEvent => Unit = _ => {}): Unit = {
     connectionAlert.onclick = fn
     connectionAlert.innerHTML = msg
     connectionAlert.classList.remove("alert-warning")
     connectionAlert.classList.remove("alert-danger")
-    connectionAlert.classList.add("alert-success")
+    connectionAlert.classList.remove("alert-success")
+    connectionAlert.classList.add(currentFlag)
     connectionAlert.classList.add("in")
   }
 
-  def warning(msg: String, fn: MouseEvent => Unit = _ => {}) = {
-    connectionAlert.onclick = fn
-    connectionAlert.innerHTML = msg
-    connectionAlert.classList.remove("alert-success")
-    connectionAlert.classList.remove("alert-danger")
-    connectionAlert.classList.add("alert-warning")
-    connectionAlert.classList.add("in")
+  def success(msg: String, fn: MouseEvent => Unit = _ => {}): Unit = {
+    makeAlert(msg, "alert-success", fn)
   }
 
-  def error(msg: String, fn: MouseEvent => Unit = _ => {}) = {
-    connectionAlert.onclick = fn
-    connectionAlert.innerHTML = msg
-    connectionAlert.classList.remove("alert-success")
-    connectionAlert.classList.remove("alert-warning")
-    connectionAlert.classList.add("alert-danger")
-    connectionAlert.classList.add("in")
+  def warning(msg: String, fn: MouseEvent => Unit = _ => {}): Unit = {
+    makeAlert(msg, "alert-warning", fn)
+  }
+
+  def error(msg: String, fn: MouseEvent => Unit = _ => {}): Unit = {
+    makeAlert(msg, "alert-danger", fn)
   }
 
   def fadeOut(after: FiniteDuration = 2.seconds) = timers.setTimeout(after) {
