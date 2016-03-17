f67babfe6ea914f8f59004b2fbd0228d76e7903c
Patryk Koryzna <patryk.koryzna@blstream.com>
2016-03-17 21:49:14 +0100

    move color selection to separate trait

--- frontend/src/main/scala/akkaviz/frontend/components/ActorStateAsNodeRenderer.scala
+++ frontend/src/main/scala/akkaviz/frontend/components/ActorStateAsNodeRenderer.scala
@@ -1,11 +1,11 @@
 package akkaviz.frontend.components
 
 import akkaviz.frontend.ActorRepository.ActorState
-import akkaviz.frontend.vis
+import akkaviz.frontend.{FancyColors, vis}
 
 import scala.scalajs.js
 
-object ActorStateAsNodeRenderer {
+object ActorStateAsNodeRenderer extends FancyColors {
 
   def render(id: String, state: ActorState): vis.Node = {
     js.Dynamic.literal(
@@ -14,38 +14,6 @@ object ActorStateAsNodeRenderer {
       label = state.label,
       color = if (state.isDead) deadColor else colorForString(state.system)
     ).asInstanceOf[vis.Node]
-  }
-
-  //http://tools.medialab.sciences-po.fr/iwanthue/index.php
-  private[this] val possibleColors = js.Array[String](
-    "#D7C798",
-    "#8AD9E5",
-    "#ECACC1",
-    "#97E3B0",
-    "#D5EB85",
-    "#F2AE99",
-    "#DFC56D",
-    "#DAECD0",
-    "#D2DCE6",
-    "#D4F0AD",
-    "#D9C0B3",
-    "#E8AD70",
-    "#DAC1E1",
-    "#82DDC9",
-    "#B8BE74",
-    "#A8C5AA",
-    "#A7C5E2",
-    "#A0D38F",
-    "#EEDF98",
-    "#ADD7D4"
-  )
-
-  @inline
-  private[this] val deadColor = "#666666"
-
-  @inline
-  private[this] def colorForString(str: String): String = {
-    possibleColors(Math.abs(str.hashCode) % possibleColors.size)
   }
 
 }
