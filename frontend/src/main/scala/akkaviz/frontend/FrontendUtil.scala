9b222fdf25214939859e598c1b17613f979bde8e
Patryk Koryzna <patryk.koryzna@blstream.com>
2016-03-17 21:49:14 +0100

    move color selection to separate trait

--- frontend/src/main/scala/akkaviz/frontend/FrontendUtil.scala
+++ frontend/src/main/scala/akkaviz/frontend/FrontendUtil.scala
@@ -41,3 +41,38 @@ object FrontendUtil {
     else Some("akka://" + path.init.mkString("/"))
   }
 }
+
+trait FancyColors {
+  //http://tools.medialab.sciences-po.fr/iwanthue/index.php
+  private[this] val possibleColors = js.Array[String](
+    "#D7C798",
+    "#8AD9E5",
+    "#ECACC1",
+    "#97E3B0",
+    "#D5EB85",
+    "#F2AE99",
+    "#DFC56D",
+    "#DAECD0",
+    "#D2DCE6",
+    "#D4F0AD",
+    "#D9C0B3",
+    "#E8AD70",
+    "#DAC1E1",
+    "#82DDC9",
+    "#B8BE74",
+    "#A8C5AA",
+    "#A7C5E2",
+    "#A0D38F",
+    "#EEDF98",
+    "#ADD7D4"
+  )
+
+  @inline
+  val deadColor = "#666666"
+
+  @inline
+  def colorForString(str: String): String = {
+    possibleColors(Math.abs(str.hashCode) % possibleColors.size)
+  }
+
+}
