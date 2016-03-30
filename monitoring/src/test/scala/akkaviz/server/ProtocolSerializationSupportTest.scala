61c81f09d40bc281a1ecad9a2bcc86f90931f1db
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-03-31 01:56:30 +0200

    Increase patience in ProtocolSerializationSupportTest, travis is really slow :(

--- monitoring/src/test/scala/akkaviz/server/ProtocolSerializationSupportTest.scala
+++ monitoring/src/test/scala/akkaviz/server/ProtocolSerializationSupportTest.scala
@@ -5,14 +5,18 @@ import akka.http.scaladsl.model.ws.BinaryMessage
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Sink, Source}
 import akka.util.ByteString
-import akkaviz.protocol.{SetEnabled, IO, Killed}
-import org.scalatest.{Matchers, FunSuite}
+import akkaviz.protocol.{IO, Killed, SetEnabled}
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{FunSuite, Matchers}
+
+import scala.concurrent.duration._
 
 class ProtocolSerializationSupportTest extends FunSuite with ScalaFutures with Matchers {
 
   private[this] implicit val system = ActorSystem()
   private[this] implicit val materializer = ActorMaterializer()
+
+  override implicit val patienceConfig: PatienceConfig = PatienceConfig(10.seconds)
 
   import ProtocolSerializationSupport._
 
